efe1de89cc6ae87946c8ca102fc8d0dfadf36446
Daniel <DanielCarrete_Dev@outlook.com>
2020-11-05 12:35:04 -0600

    los componentes funcionan donde los deje

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import Page from './page/page'
+import React, { Component } from 'react'
+import Files from './components/files'
+import Stats from './components/stats'
+import Server from './components/server_on'
 
-function App() {
-  return (
-    <Page/>
-  );
+export class App extends Component {
+
+    constructor(props){
+        super(props)
+        this.handleServerChange =this.handleServerChange.bind(this)
+        this.state = {
+            server_on : false
+        }
+    }
+
+    handleServerChange(e) {
+        this.setState({server_on: e});
+        console.log(this.state.server_on)
+    }
+
+    render() {
+        const server_on = this.state.server_on;
+        return (
+            <React.Fragment>
+                {
+                    server_on ?
+                        <Server changeServerOn = {this.handleServerChange}/>
+                    :
+                        <div className='App'>
+                            <Files/>
+                            <Stats changeServerOn = {this.handleServerChange}/>
+                        </div>
+                }
+                
+                
+
+            </React.Fragment>
+        )
+    }
 }
 
-export default App;
+export default App
